96f95df581eae338408d76d2486958fe8c25e9ca
poorva25csu178 <poorva25csu178@ncuindia.edu>
2025-11-02 22:14:06 +0530

    Implement integer swap using 4 different methods

--- Q4.c
+++ Q4.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
-
 int main() {
+    
     int a, b, temp;
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    printf("\nOriginal: a = %d, b = %d\n", a, b);
-
-    // Method 1: Using temp variable
+    printf("\nOriginal: a = %d, b = %d\n", a, 
+        
     temp = a;
     a = b;
     b = temp;
     printf("After temp swap: a = %d, b = %d\n", a, b);
 
-    // Method 2: Using arithmetic
     a = a + b;
     b = a - b;
     a = a - b;
     printf("After arithmetic swap: a = %d, b = %d\n", a, b);
 
-    // Method 3: Using XOR
     a = a ^ b;
     b = a ^ b;
     a = a ^ b;
     printf("After XOR swap: a = %d, b = %d\n", a, b);
 
-    // Method 4: Using pointers
     int *p1 = &a, *p2 = &b, t;
     t = *p1;
     *p1 = *p2;
